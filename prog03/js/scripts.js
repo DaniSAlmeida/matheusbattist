e9b77480c5054647738f4cb79090f0f0993e5c8d
Daniele Almeida <108015547+DaniSAlmeida@users.noreply.github.com>
2022-07-22 18:49:25 +0100

    particularidades js

---
+++ prog03/js/scripts.js
@@ -0,0 +1,23 @@
+console.log('Primeiro');
+
+console.log('Último');
+
+
+
+var a = 1;
+
+a = 'teste';
+
+console.log(a);
+
+// Este é um comentário
+
+/*
+
+Este é um comentario de mais linhas
+de 
+múltiplas 
+linhas
+
+
+*/
