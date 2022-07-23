26dc473c16d79df22850bf8f89a015bea7da0160
Daniele Almeida <108015547+DaniSAlmeida@users.noreply.github.com>
2022-07-23 11:57:33 +0100

    aprendendo sobre variáveis

---
+++ prog04/js/scripts.js
@@ -0,0 +1,22 @@
+var teste = 1;
+
+console.log(teste);
+
+teste = 'Daniele';
+
+console.log(teste);
+
+var meusobrenome = 'Almeida Boechat'
+console.log(meusobrenome);
+
+/*
+
+var meuPrimeiroNome = 'Dani' // o nome dessa forma é camelCase
+
+*/
+
+let testando = 1 ;
+const ola = 2 ;
+
+console.log(testando);
+console.log(ola);
