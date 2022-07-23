11edc1cc2c40b17ea94d951263b2ba654b443dbd
Daniele Almeida <108015547+DaniSAlmeida@users.noreply.github.com>
2022-07-23 13:18:29 +0100

    Aprendendo sobre Strings

---
+++ prog06/js/scripts.js
@@ -0,0 +1,20 @@
+var nome = 'Daniele';
+var sobrenome = ' Almeida Boechat ';
+
+var nomeCompleto = nome + '' + sobrenome;
+console.log(nomeCompleto);
+
+console.log(nome);
+console.log(typeof nome); 
+
+console.log(typeof '523500')
+
+var frase = ' Esta é uma frase complexa.';
+console.log(frase);
+console.log(typeof frase);
+
+console.log(nome + "" + frase);
+
+document.write('Ela disse: "Olá!"') // o mesmo pode ser feito ao contrário com as aspas.
+
+console.log('Este número: ' + '432'); // não assume como soma, e sim de Concatenação.
