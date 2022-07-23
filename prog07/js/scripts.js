bacd5325052710c65245ee9df14e3ffc0da8a2f3
Daniele Almeida <108015547+DaniSAlmeida@users.noreply.github.com>
2022-07-23 18:26:26 +0100

    aprendendo Boolean

---
+++ prog07/js/scripts.js
@@ -0,0 +1,16 @@
+var verdadeiro = true;
+
+console.log(verdadeiro);
+console.log(typeof verdadeiro);
+
+//tb pode ser em inglês
+
+console.log(typeof true);
+console.log(typeof false);
+
+// aii em cima 
+
+var falso = false;
+
+console.log(falso);
+console.log(typeof falso);
