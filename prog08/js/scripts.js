13b8e2517bc0ff1014f0065a754e7f3a0e4c3a33
Daniele Almeida <108015547+DaniSAlmeida@users.noreply.github.com>
2022-07-23 19:00:48 +0100

    Aprendendo Null e Undefined

---
+++ prog08/js/scripts.js
@@ -0,0 +1,15 @@
+// Hoisting - Içamento
+console.log(sobrenome); // ele declara avariável , mas não inicia ela .
+console.log(numero);
+
+var nome = null;
+var sobrenome = 'Almeida Boechat';
+
+console.log(nome);
+console.log(sobrenome);
+
+nome = 'Daniele';
+
+console.log(nome);
+
+var numero = 5;
