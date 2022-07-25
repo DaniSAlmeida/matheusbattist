bcfa6893865b277488ea5eef10e711eede8e5c89
Daniele Almeida <108015547+DaniSAlmeida@users.noreply.github.com>
2022-07-25 19:03:29 +0100

    Aprendendo Op Condicionais If

---
+++ prog11/js/scripts.js
@@ -0,0 +1,13 @@
+var idade = 18;
+
+console.log ("Antes do if")
+
+if(idade >=18) {
+    console.log("Pode fazer a carteira de Habilitação")
+}
+
+if(idade > 15) {
+    console.log("Precisa esperar 3 anos ainda!")
+}
+
+console.log("Depois do if")
