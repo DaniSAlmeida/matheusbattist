6f088bfabe2f2915b3e76f3757525726e251df98
Daniele Almeida <108015547+DaniSAlmeida@users.noreply.github.com>
2022-07-25 18:24:07 +0100

    Aprendendo sobre Arrays

---
+++ prog10/js/scripts.js
@@ -0,0 +1,16 @@
+var arr = [5, "Daniele", true, {teste: 1, teste: 2}]; // obj => {}
+
+console.log(arr);
+
+var arr2 = [2,3,4,5,6];
+
+console.log(arr2);
+
+console.log(arr[1]);  // acessar algum dos elementos do array.
+console.log(arr2[0]);  // acessar algum dos elementos do array.
+
+arr[4] = 10; // acresecentamos elementos ao array.
+
+arr[0] = "Teste" // modificamos algum elemento do array.
+
+console.log(arr);
