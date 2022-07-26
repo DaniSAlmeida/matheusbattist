60a95e6bab3da01a16ff259fcbf944f7174ea432
Daniele Almeida <108015547+DaniSAlmeida@users.noreply.github.com>
2022-07-26 17:56:25 +0100

    Aprendendo sobre comparação de Tipo e Valor

---
+++ prog14/js/scripts.js
@@ -0,0 +1,13 @@
+var numero = 'cinco';
+
+if(numero === 5) {
+    console.log("O número é 5");
+} else {
+    console.log("É uma string")
+}
+
+
+// Se diferente de 5, se eu add aspas la no if de cima , deixa passar como string
+if(numero !== 5) {
+    console.log("Não é o número 5 do tipo Number")
+}
