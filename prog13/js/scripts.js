b9a4951089d91214b610111f1bf649e1bc3b0f9c
Daniele Almeida <108015547+DaniSAlmeida@users.noreply.github.com>
2022-07-26 17:29:04 +0100

    Aprendendo sobre Else if e Else

---
+++ prog13/js/scripts.js
@@ -0,0 +1,25 @@
+var nome = "Daniele";
+
+if(nome == "Felipe") {
+    console.log("O nome dele é Felipe");
+} else if(nome== "Daniele") {
+    console.log("O nome é Daniele");
+} else {
+    console.log("Ela(e) possui outro nome!")
+}
+
+
+var idade = 19;
+
+if(idade >= 20) {
+    console.log("Ele pode entrar na festa!");
+} else if(idade >= 18) {
+    console.log("Ele só pode entrar com  autorização")
+}
+
+
+if(nome == "Daniele") {
+    console.log("teste");
+} else {
+    console.log("testando");
+}
