fa30c5b88021cedf8affbbaeb2157a8fff4175ef
Daniele Almeida <108015547+DaniSAlmeida@users.noreply.github.com>
2022-07-28 13:26:12 +0100

    Aprendendo sobre o Op Lógico Not !

---
+++ prog17/js/scripts.js
@@ -0,0 +1,16 @@
+if(true) {
+    console.log("Passou")
+}
+
+var nome = "Daniele";
+
+/*
+if(nome == "Daniele") {
+    console.log("Ok");
+}
+*/
+if(!(nome == "Daniele")) {
+    console.log("Ok");
+}
+
+// O Operador Not apenas inverte o True e o False 
