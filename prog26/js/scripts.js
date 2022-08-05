e98b18b55933ddea93c96c8dc4c0f64acdab46dc
Daniele Almeida <108015547+DaniSAlmeida@users.noreply.github.com>
2022-08-05 17:25:05 +0100

    Aprendendo sobre métodos numéricos

---
+++ prog26/js/scripts.js
@@ -0,0 +1,26 @@
+// parseFloat 10.0
+
+console.log(parseFloat ('12.999'));
+console.log(Number.parseFloat('12.999'));
+
+// parseInt
+
+console.log(parseInt('10'));
+console.log(parseInt(16.96));
+
+// toFixed
+
+console.log(23.99.toFixed(1)); // ele arredondou e deixou com uma casa decimal dps da vírgula.
+
+// isNaN - pra verificar se um Not a Number
+
+console.log(isNaN("teste"));
+console.log(isNaN(12));
+console.log(isNaN("14"));
+
+// MAX_VALUE e MIN_VALUE - O máximo e o mínimo que conseguimos chegar no JS.
+
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+
+console.log(2.7976931348623157e+308)
