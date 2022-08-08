0e6bccb9df2764e4717f1dd8e28c2dd36d9dd6c7
Daniele Almeida <108015547+DaniSAlmeida@users.noreply.github.com>
2022-08-08 17:09:42 +0100

    Aprendendo métodos de String parte 1

---
+++ prog27/js/scripts.js
@@ -0,0 +1,29 @@
+// length
+
+var nome = "Daniele";
+
+console.log(nome.length);
+
+var obj = "bola";
+
+console.log(obj.length);
+
+// indexOf
+
+console.log(nome[2]);
+
+var frase = "O rato roeu a roupa do rei de Roma";
+
+console.log(frase.indexOf("roeu"));
+
+// slice (pedaço)
+
+var roeu= frase.slice(7, 11);
+
+console.log(roeu);
+
+// replace
+
+var novaFrase = frase.replace("roeu", "teste");
+
+console.log(novaFrase);
